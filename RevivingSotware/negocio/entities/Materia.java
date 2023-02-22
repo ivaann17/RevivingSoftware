6f5eda60d4d761641ac3086279f5d757a65b665b
ivaann17 <ivan.munoz3@alu.uclm.es>
2023-02-22 12:38:32 +0100

    Generación de código esqueleto.

---
+++ RevivingSotware/negocio/entities/Materia.java
@@ -0,0 +1,13 @@
+package negocio.entities;
+
+import java.sql.Date;
+
+public class Materia {
+
+	Profesor responsable;
+	private String nombre;
+	private double horas;
+	private Date fechaInicio;
+	private Date fechaFin;
+
+}
