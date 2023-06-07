821113b7f4180d9e4816afb0234ded375e7a3a8f
ivaann17 <ivan.munoz3@alu.uclm.es>
2023-06-07 15:08:41 +0200

    Proyecto final libre de error

--- src/test/java/negocio/controllers/GestorMatriculacionTest.java
+++ src/test/java/negocio/controllers/GestorMatriculacionTest.java
@@ -11,15 +11,22 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import main.java.negocio.controllers.GestorMatriculacion;
+import main.java.negocio.controllers.GestorPropuestasCursos;
+import main.java.negocio.entities.Centro;
+import main.java.negocio.entities.CursoPropio;
+import main.java.negocio.entities.EstadoCurso;
 import main.java.negocio.entities.Matricula;
 import main.java.negocio.entities.ModoPago;
+import main.java.negocio.entities.TipoCurso;
 import main.java.persistencia.GestorBD;
 
 class GestorMatriculacionTest {
+	
+	
 	// CP1
 	Matricula mat1() {
 		return new Matricula(1, "Ivan", "Muñoz", ModoPago.TRANSFERENCIA, Date.valueOf("2023-05-01"), "02023784L",
-				1233.08, 86);
+				1233.08, 67);
 	}
 
 	boolean matCorrecta(Matricula mat) {
@@ -39,7 +46,13 @@ class GestorMatriculacionTest {
 	// CP3
 	Matricula mat3() {
 		return new Matricula(-15, "Ivan", "Muñoz", ModoPago.TRANSFERENCIA, Date.valueOf("2023-05-01"), "02023784L",
-				1233.08, 86);
+				1233.08, 67);
+	}
+	
+	CursoPropio curso1() {
+		return new CursoPropio(new Centro("CAMPUS_TALAVERA").getNombre(), EstadoCurso.PROPUESTO, TipoCurso.MASTER,
+				"12345678P", "98765432S", 67, "Curso Test", 120, new Date(1 / 5 / 2023), new Date(1 / 6 / 2023), 100.0,
+				1, "Mensaje 1");
 	}
 
 	@BeforeEach
@@ -52,9 +65,12 @@ class GestorMatriculacionTest {
 	void testRealizarMatriculacion() throws SQLException {
 		// A
 		Matricula mat = mat1();
+		CursoPropio c = curso1();
+		GestorPropuestasCursos.realizarPropuestaCurso(c);
 		GestorMatriculacion.realizarMatriculacion(mat);
 		assertEquals(matCorrecta(mat), GestorMatriculacion.existe(mat.getIDCurso(), mat.getDni()));
 		GestorMatriculacion.eliminarMat(mat.getIDCurso(), mat.getDni());
+		GestorPropuestasCursos.eliminarCurso(c);
 
 		// B
 		mat = mat2();
@@ -72,9 +88,12 @@ class GestorMatriculacionTest {
 	void testExisteMatricula() throws SQLException {
 		// A
 		Matricula mat = mat1();
+		CursoPropio c = curso1();
+		GestorPropuestasCursos.realizarPropuestaCurso(c);
 		GestorMatriculacion.realizarMatriculacion(mat);
 		assertTrue(GestorMatriculacion.existe(mat.getIDCurso(), mat.getDni()));
 		GestorMatriculacion.eliminarMat(mat.getIDCurso(), mat.getDni());
+		GestorPropuestasCursos.eliminarCurso(c);
 
 		// B
 		mat = mat2();
@@ -92,12 +111,15 @@ class GestorMatriculacionTest {
 	void testEliminarMatricula() throws SQLException {
 		// A
 		Matricula mat = mat1();
+		CursoPropio c = curso1();
+		GestorPropuestasCursos.realizarPropuestaCurso(c);
 		GestorMatriculacion.realizarMatriculacion(mat);
 
 		boolean existeMatAntes = GestorMatriculacion.existe(mat.getIDCurso(), mat.getDni());
 		assertTrue(existeMatAntes);
 		// B
 		GestorMatriculacion.eliminarMat(mat.getIDCurso(), mat.getDni());
+		GestorPropuestasCursos.eliminarCurso(c);
 
 		boolean existeMatDespues = GestorMatriculacion.existe(mat.getIDCurso(), mat.getDni());
 		assertFalse(existeMatDespues);
